4b280e6baef03b29a023d6dce95cdb679736e760
Indunil Aravinda <indunilaravindapro@gmail.com>
2022-07-13 21:11:34 +0530

    Completed simple game to guess random number

--- NumberGuesser/Program.cs
+++ NumberGuesser/Program.cs
@@ -8,7 +8,59 @@ class NumberGuess
 
         int numberToGuess = new Random().Next(20);
 
+        // Game Instructions
+        Console.WriteLine("Welcome to Number Guesser. You need to guess the number that I'm thinking of within the range of 0 & 20");
+        Console.WriteLine("----------------------------------------------------------------------------------------------------------");
 
-        Console.WriteLine("It Works");
+        // Starting the loop
+        bool guessedIncorrect = true;
+        int count = 1;
+        while (guessedIncorrect)
+        {
+            Console.WriteLine("Enter your guess, or -1 to give up");
+
+            // recording the guess
+            string guess = Console.ReadLine();
+
+            int guessNumber = -1;
+            try
+            {
+                guessNumber = int.Parse(guess);
+            }
+            catch
+            {
+                Console.WriteLine("Please enter a valid number");
+
+            }
+
+            // Checking the guess
+            if (numberToGuess.Equals(guessNumber))
+            {
+                Console.WriteLine($"Well Done! You guessed it within {count} attempt");
+                guessedIncorrect = false;
+            }
+            else if (guessNumber.Equals(-1))
+            {
+                break;
+            }
+            else if (guessNumber > numberToGuess)
+            {
+
+                Console.WriteLine("Lower than " + guessNumber);
+                count++;
+            }
+            else if (guessNumber < numberToGuess)
+            {
+
+                Console.WriteLine("Higher than " + guessNumber);
+                count++;
+            }
+            else
+            {
+                Console.WriteLine("Something is wrong");
+                count++;
+            }
+        }
+
     }
 }
