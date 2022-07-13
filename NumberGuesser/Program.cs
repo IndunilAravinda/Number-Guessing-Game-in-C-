2b14fe4a2e4f7cea4fbb9173cbd0cc33dff0611e
Indunil Aravinda <indunilaravindapro@gmail.com>
2022-07-13 20:49:58 +0530

    Number Guessing game in c# init

---
+++ NumberGuesser/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class NumberGuess
+{
+    static void Main(string[] args)
+    {
+        ///<summary> Choosing nuber range between 0-20 </summary>
+
+        int numberToGuess = new Random().Next(20);
+
+
+        Console.WriteLine("It Works");
+    }
+}
